eb4918b4b439cf125c21a1e24e0d3ecad36f4396
hanyu7377 <80314661+hanyu7377@users.noreply.github.com>
2023-06-19 21:24:37 -0400

    Add files via upload

--- The select clasue.sql
+++ The select clasue.sql
@@ -1,8 +1,21 @@
 select * from store.customers 
-where customer_id = 1
-;
+where customer_id = 1;
 
 select * from store.customers
 order by first_name;
 
-select * from store.customers
+select last_name , 
+first_name , 
+points ,
+(points + 10) + 100  as 'discount_factor' 
+from store.customers;
+
+select distinct state from store.customers;
+select  state from store.customers;
+
+
+-- return all the products
+--  name
+-- unit price
+-- new price (unit price * 1.1)
+select  name ,unit_price, unit_price * 1.1 as new_price from store.products
