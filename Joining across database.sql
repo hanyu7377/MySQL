e53664b86baf3f8d087aceaca13876bb4c9ec243
hanyu7377 <80314661+hanyu7377@users.noreply.github.com>
2023-07-01 22:02:46 -0400

    Add files via upload

---
+++ Joining across database.sql
@@ -0,0 +1,7 @@
+select * from  store.order_items oi
+join sql_inventory.products p on oi.product_id = p.product_id;
+
+
+use sql_inventory;
+select * from  store.order_items oi
+join sql_inventory.products p on oi.product_id = p.product_id;
